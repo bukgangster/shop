512e1859761ec95a43830736283fa5b30ba12317
yangc <limyangchae@kakao.com>
2023-08-22 17:54:19 +0900

    header, footer, content 작업

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,10 +2,16 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long>{
+// Predicate? '이 조건이 맞다'고 판단하는 근거를 함수로 제공하는 것.  =>  QuerydslPredicateExecutor 인터페이스 상속
+
+public interface ItemRepository extends JpaRepository<Item, Long>
+        , QuerydslPredicateExecutor<Item>{
+
+
 
     // find + (엔티티 이름) + By + 변수이름
     List<Item> findByItemNm(String itemNm);
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,24 +1,47 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-//@SpringBootTest
-//@TestPropertySource(locations="classpath:application-test.yml")
+@SpringBootTest
+@TestPropertySource(locations="classpath:application-test.yml")
 class ItemRepositoryTest {
 
-//    @Autowired
-//    ItemRepository itemRepository;
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void createItemTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 
 //    @Test
 //    @DisplayName("상품 저장 테스트")
@@ -35,19 +58,19 @@ class ItemRepositoryTest {
 //        System.out.println(saveItem.toString());
 //    }
 
-//    public void createItemList(){
-//        for(int i=1; i<=10; i++){
-//            Item item = new Item();
-//            item.setItemNm("테스트상품" + i);
-//            item.setPrice(10000 + i);
-//            item.setItemDetail("테스트 상품 상세 설명" + i);
-//            item.setItemSellStatus(ItemSellStatus.SELL);
-//            item.setStockNumber(100);
-//            item.setRegTime(LocalDateTime.now());
-//            item.setUpdateTime(LocalDateTime.now());
-//            Item saveItem = itemRepository.save(item);
-//        }
-//    }
+    public void createItemList(){
+        for(int i=1; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm("테스트상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item saveItem = itemRepository.save(item);
+        }
+    }
 
 //    @Test
 //    @DisplayName("상품명 조회 테스트")
